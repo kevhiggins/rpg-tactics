673cf7a519c99cb904b722a5bc234509122e1188
Kevin Higgins <kev.higgins@gmail.com>
2016-07-31 23:26:01 -0500

    Enh: Add cursor for tile selection, and the ability to move it around

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+    public GameObject tileSelection;
+
+
+	// Use this for initialization
+	void Start () {
+        // Create the cursor at 16px and 16px
+        GameObject selection = Instantiate(tileSelection, new Vector3(16, -16, 0), Quaternion.identity) as GameObject;
+
+        // Potentially do something like this to pass map data to selection scripts
+		//selection.SendMessage("InitMap", currentMap);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

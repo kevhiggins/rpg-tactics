ac1b7a5ce4b645e19b554ed610a1606dd7685365
Kevin Higgins <kev.higgins@gmail.com>
2016-08-15 01:46:14 -0500

    Enh: Set the damage label for the target confirmation widget

--- Assets/Scripts/Widgets/TargetActionBoxWidget.cs
+++ Assets/Scripts/Widgets/TargetActionBoxWidget.cs
@@ -2,6 +2,7 @@
 using Assets.Scripts.Unity;
 using Rpg.Unit;
 using UnityEngine;
+using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
 namespace Rpg.Widgets
@@ -20,6 +21,8 @@ namespace Rpg.Widgets
             var targetUnitPanel = GameObjectHelper.FindChildByName(canvas, "TargetUnitInfo");
             var actionInfo = GameObjectHelper.FindChildByName(canvas, "ActionInfo");
 
+            ReplaceTextData(actionInfo, "DamageValue", "{dmg}", sourceUnit.Damage.ToString());
+
             // Populate TargetActionBox data
             UnitInfoWidget.PopulateUnitInfoDisplay(sourceUnitPanel, sourceUnit);
             UnitInfoWidget.PopulateUnitInfoDisplay(targetUnitPanel, targetUnit);
@@ -33,6 +36,15 @@ namespace Rpg.Widgets
 
         public override void HandleInput()
         {
+        }
+
+        // TODO Centralize this
+        protected static void ReplaceTextData(GameObject parent, string textFieldName, string targetData, string newData)
+        {
+            var hitPointObject = GameObjectHelper.FindChildByName(parent, textFieldName);
+            var textScript = hitPointObject.GetComponent<Text>();
+
+            textScript.text = textScript.text.Replace(targetData, newData);
         }
     }
 }

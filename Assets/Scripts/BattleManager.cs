ff9a8c021dcfd0c8d5236d09262eeb2fcf2c565f
Kevin Higgins <kev.higgins@gmail.com>
2016-08-16 02:47:00 -0500

    Enh: Make popcanvas appear and then destroy itself

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,15 +1,29 @@
-﻿using Rpg.Unit;
+﻿using Assets.Scripts.Unity;
+using Rpg.Unit;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Rpg
 {
     public class BattleManager : MonoBehaviour
     {
+        public GameObject popCanvas;
+
         public void AttackUnit(IUnit sourceUnit, IUnit targetUnit)
         {
             sourceUnit.Attack();
             targetUnit.TakeDamage(sourceUnit.Damage);
+            DisplayDamagePop(targetUnit, sourceUnit.Damage);
             sourceUnit.EndTurn();
+        }
+
+        public void DisplayDamagePop(IUnit targetUnit, int damage)
+        {
+            var popObject = Instantiate(popCanvas);
+            popObject.transform.position = targetUnit.GetGameObject().transform.position;
+            var textObject = GameObjectHelper.FindChildByName(popObject, "FloatingDMG");
+            var textScript = textObject.GetComponent<Text>();
+            textScript.text = textScript.text.Replace("{dmg}", damage.ToString());
         }
     }
 }
---
+++ Assets/Scripts/Unity/AnimationAutoDestroy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Rpg.Unity
+{
+    public class AnimationAutoDestroy : MonoBehaviour
+    {
+        public float delay = 0f;
+        public bool destroyParent = false;
+
+        // Use this for initialization
+        void Start()
+        {
+            var target = gameObject;
+            if (destroyParent)
+            {
+                target = gameObject.transform.parent.gameObject;
+            }
+            Destroy(target, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length + delay);
+        }
+    }
+}

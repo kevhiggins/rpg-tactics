5e62f098d2c4951ff470c5a3d5622d1109337496
Kevin Higgins <kev.higgins@gmail.com>
2016-08-18 11:13:01 -0500

    Bug: Fix issue where the battle would never complete if the death animation completes before the attack animation

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -9,21 +9,6 @@ namespace Rpg
     {
         public void AttackUnit(IUnit sourceUnit, IUnit targetUnit)
         {
-
-            DeathCompleteHandler deathCompleteHandler = null;
-            targetUnit.OnDeathComplete += deathCompleteHandler = () =>
-            {
-                // Unregister callback.
-                targetUnit.OnDeathComplete -= deathCompleteHandler;
-
-                // If the target unit is dead, and they were worth experience, grant it to the attacking unit.
-                if (targetUnit.IsDead && targetUnit.ExperienceWorth > 0)
-                {
-                    sourceUnit.GainExperience(targetUnit.ExperienceWorth);
-                }
-                sourceUnit.EndTurn();
-            };
-
             // When the hit lands, apply damage to the target unit.
             AttackHitHandler attackHitHandler = null;
             sourceUnit.OnAttackHit += attackHitHandler = () =>
@@ -32,21 +17,39 @@ namespace Rpg
                 sourceUnit.OnAttackHit -= attackHitHandler;
 
                 targetUnit.TakeDamage(sourceUnit.Damage);
-            };
 
-            // When the attack is complete, check if the enemy is dead, award experience if necessary, and end the turn.
-            AttackCompleteHandler attackCompleteHandler = null;
-            sourceUnit.OnAttackComplete += attackCompleteHandler = () =>
-            {
-                // Unregister the callback
-                sourceUnit.OnAttackComplete -= attackCompleteHandler;
-
-                // If the target unit is not dead, then end the turn. Otherwise, the death handler will take care of this.
-                if (targetUnit.IsDead == false)
+                // If the target is dead, then attach the deathCompleteHandler to complete the turn.
+                if (targetUnit.IsDead)
                 {
-                    sourceUnit.EndTurn();
+                    DeathCompleteHandler deathCompleteHandler = null;
+                    targetUnit.OnDeathComplete += deathCompleteHandler = () =>
+                    {
+                        // Unregister callback.
+                        targetUnit.OnDeathComplete -= deathCompleteHandler;
+
+                        // If the target unit is dead, and they were worth experience, grant it to the attacking unit.
+                        if (targetUnit.ExperienceWorth > 0)
+                        {
+                            sourceUnit.GainExperience(targetUnit.ExperienceWorth);
+                        }
+                        sourceUnit.EndTurn();
+                    };
+                }
+                else
+                {
+                    // When the attack is complete, check if the enemy is dead, award experience if necessary, and end the turn.
+                    AttackCompleteHandler attackCompleteHandler = null;
+                    sourceUnit.OnAttackComplete += attackCompleteHandler = () =>
+                    {
+                        // Unregister the callback
+                        sourceUnit.OnAttackComplete -= attackCompleteHandler;
+
+                        // If the target unit is not dead, then end the turn. Otherwise, the death handler will take care of this.
+                        sourceUnit.EndTurn();
+                    };
                 }
             };
+
 
             // Ideally, if the enemy is dead, we should wait for the death animation to finish to end the turn.
             // Otherwise, just end the turn after the attack.
